35ba03f1256d3b6c19f4bae201c1391e12126145
AteCookie <k024g1012@g.neec.ac.jp>
2025-06-20 14:12:44 +0900

    1

--- main.cpp
+++ main.cpp
@@ -11,21 +11,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, kWindowWidth, kWindowHeight);
 
-	// キー入力結果を受け取る箱
-//	char keys[256] = {0};
-//	char preKeys[256] = {0};
 
 	/// 初期処理
 	GameManager* gameManager = new GameManager();
 
-	// ウィンドウの×ボタンが押されるまでループ
-//	while (Novice::ProcessMessage() == 0) {
-		// フレームの開始
-    //Novice::BeginFrame();
-
-		// キー入力を受け取る
-	//	memcpy(preKeys, keys, 256);
-	//	Novice::GetHitKeyStateAll(keys);
 
 		///
 		/// ↓更新処理ここから
@@ -46,14 +35,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 
 		// フレームの終了
-	    //Novice::EndFrame();
+	
 		delete gameManager;
-
-		// ESCキーが押されたらループを抜ける
-//		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
-//			break;
-//		}
-//	}
 
 	// ライブラリの終了
 	Novice::Finalize();
